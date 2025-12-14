8e5e34017bd9d25a22eadba4c7c5eccdd7093f8f
ljkhyeong <jolri24@gmail.com>
2025-12-14 21:06:04 +0900

    [Application] shortUrl 생성 및 조회, shortKey로 OriginalUrl 조회 기능 추가

--- src/main/java/com/personal/short_url/application/ShortUrlService.java
+++ src/main/java/com/personal/short_url/application/ShortUrlService.java
@@ -1,2 +1,35 @@
-package com.personal.short_url.application;public class ShortUrlService {
+package com.personal.short_url.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.personal.short_url.domain.Base62Utils;
+import com.personal.short_url.domain.entity.ShortUrl;
+import com.personal.short_url.infrastructure.NotFoundException;
+import com.personal.short_url.infrastructure.persistence.ShortUrlRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ShortUrlService {
+
+	private final ShortUrlRepository shortUrlRepository;
+
+	@Transactional
+	public String generateShortUrl(String originalUrl) {
+		return shortUrlRepository.findByOriginalUrl(originalUrl)
+			.map(shortUrl -> Base62Utils.encodeToKey(shortUrl.getId()))
+			.orElseGet(() -> {
+				ShortUrl saved = shortUrlRepository.save(new ShortUrl(originalUrl));
+				return Base62Utils.encodeToKey(saved.getId());
+			});
+	}
+
+	@Transactional
+	public String getOriginalUrl(String shortKey) {
+		return shortUrlRepository.findById(Base62Utils.decodeToId(shortKey))
+			.orElseThrow(() -> new NotFoundException("original URL 미존재"))
+			.getOriginalUrl();
+	}
 }
---
+++ src/test/java/com/personal/short_url/application/ShortUrlServiceTest.java
@@ -0,0 +1,63 @@
+package com.personal.short_url.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.personal.short_url.domain.Base62Utils;
+import com.personal.short_url.domain.entity.ShortUrl;
+import com.personal.short_url.infrastructure.persistence.ShortUrlRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ShortUrlServiceTest {
+	@Mock
+	ShortUrlRepository shortUrlRepository;
+	@InjectMocks
+	ShortUrlService shortUrlService;
+
+	@DisplayName("URL 단축 요청 시 저장 후 인코딩된 키를 반환한다")
+	@Test
+	void createShortUrl() {
+		// given
+		String originalUrl = "https://www.google.com";
+		ShortUrl savedEntity = new ShortUrl(originalUrl);
+		ReflectionTestUtils.setField(savedEntity, "id", 100L);
+
+		given(shortUrlRepository.save(any())).willReturn(savedEntity);
+
+		// when
+		String shortKey = shortUrlService.generateShortUrl(originalUrl);
+
+		// then
+		assertThat(shortKey).isNotNull();
+		verify(shortUrlRepository).save(any());
+	}
+
+	@DisplayName("이미 존재하는 URL이면 기존의 ShortKey를 반환해야 한다")
+	@Test
+	void createShortUrl_duplicate() {
+		// given
+		String url = "https://google.com";
+		ShortUrl existingEntity = new ShortUrl(url);
+		ReflectionTestUtils.setField(existingEntity, "id", 100L);
+
+		given(shortUrlRepository.findByOriginalUrl(url)).willReturn(Optional.of(existingEntity));
+
+		// when
+		String shortKey = shortUrlService.generateShortUrl(url);
+
+		// then
+		verify(shortUrlRepository, never()).save(any());
+		assertThat(shortKey).isEqualTo(Base62Utils.encodeToKey(100L));
+	}
+}
