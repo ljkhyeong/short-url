b7766906e50497a0f1763b81374b18d9a6d41ba4
ljkhyeong <jolri24@gmail.com>
2025-12-14 21:07:50 +0900

    [Domain] ShortUrl Entity 추가

---
+++ src/main/java/com/personal/short_url/domain/entity/ShortUrl.java
@@ -0,0 +1,26 @@
+package com.personal.short_url.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ShortUrl {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String originalUrl;
+
+	public ShortUrl(String originalUrl) {
+		this.originalUrl = originalUrl;
+	}
+}
