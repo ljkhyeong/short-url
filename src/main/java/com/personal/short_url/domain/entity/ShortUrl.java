e88aa0ea3a281d4498fca1aa6d40a5400fcfe7ff
ljkhyeong <jolri24@gmail.com>
2025-12-15 20:48:36 +0900

    [DB] Entity Index 튜닝

--- src/main/java/com/personal/short_url/domain/entity/ShortUrl.java
+++ src/main/java/com/personal/short_url/domain/entity/ShortUrl.java
@@ -5,11 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "short_url", indexes = @Index(name = "idx_original_url", columnList = "originalUrl", unique = true))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ShortUrl {
@@ -17,7 +20,7 @@ public class ShortUrl {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 2048)
 	private String originalUrl;
 
 	public ShortUrl(String originalUrl) {
