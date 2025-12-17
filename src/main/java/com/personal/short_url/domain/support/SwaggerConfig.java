e81e7f7351b6b8991a4d348d27fded6db568753c
ljkhyeong <jolri24@gmail.com>
2025-12-17 23:13:18 +0900

    [Docs] SpringDoc(Swagger) 도입으로 API 문서 자동화 (README 관리 비용 제거)

---
+++ src/main/java/com/personal/short_url/domain/support/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.personal.short_url.domain.support;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Short URL API")
+				.description("단축 URL 생성 및 리다이렉트 API 명세")
+				.version("v1.0"));
+	}
+}
