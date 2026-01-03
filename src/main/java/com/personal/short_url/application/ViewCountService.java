fa1fe23bd2143889b5f3d3eee89df6a5e72f12f7
ljkhyeong <jolri24@gmail.com>
2026-01-03 22:24:27 +0900

    [Async] Async에 사용될 스레드풀 설정 추가

--- src/main/java/com/personal/short_url/application/ViewCountService.java
+++ src/main/java/com/personal/short_url/application/ViewCountService.java
@@ -7,16 +7,19 @@ import org.springframework.transaction.annotation.Transactional;
 import com.personal.short_url.infrastructure.persistence.ShortUrlRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ViewCountService {
 
 	private final ShortUrlRepository shortUrlRepository;
 
-	@Async
+	@Async("taskExecutor")
 	@Transactional
 	public void increaseViewCount(Long shortUrlId) {
+		log.info("비동기 조회수 증가 실행 중... Thread: {}", Thread.currentThread().getName());
 		shortUrlRepository.increaseViewCount(shortUrlId);
 	}
 }
---
+++ src/main/java/com/personal/short_url/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.personal.short_url.infrastructure.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+	@Bean(name = "taskExecutor")
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(50);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Async-Executor-");
+		executor.initialize();
+		return executor;
+	}
+}
